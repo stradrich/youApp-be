50a2752450cb9a66bca634a939829f382b9cb3ef
Aldrich <aldrichpinso@gmail.com>
2025-08-18 14:52:08 +0800

    update: user schema

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,16 +1,17 @@
-import { Schema, SchemaFactory, Prop } from "@nestjs/mongoose";
+// src/schemas/user.schema.ts
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document } from "mongoose";
 
-@Schema({ timestamps: true })
+@Schema()
 export class User extends Document {
-    @Prop({ required: true, unique: true })
-    email: string;
+  @Prop({ required: true })
+  email!: string;
 
-    @Prop({ required: true, unique: true})
-    username: string;
+  @Prop({ required: true })
+  username!: string;
 
-    @Prop({ required: true})
-    password: string;
+  @Prop({ required: true })
+  password!: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
