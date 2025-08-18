aff51dde02392a584b2d59d9278851620cb67369
Aldrich <aldrichpinso@gmail.com>
2025-08-18 14:52:02 +0800

    update: profile schema

--- src/schemas/profile.schema.ts
+++ src/schemas/profile.schema.ts
@@ -1,40 +1,40 @@
 import { Schema, SchemaFactory, Prop } from "@nestjs/mongoose";
 import { Document, Types } from "mongoose";
 
-@Schema({ timestamps: true})
+@Schema({ timestamps: true })
 export class Profile extends Document {
-    @Prop({ type: Types.ObjectId, ref: 'User', required: true, unique: true })
-    userId: Types.ObjectId;
+  @Prop({ type: Types.ObjectId, ref: 'User', required: true, unique: true })
+  userId!: Types.ObjectId;
 
-    @Prop({ required: true, unique: true})
-    username: string;
+  @Prop({ required: true, unique: true })
+  username!: string;
 
-    @Prop()
-    age?: number;
+  @Prop()
+  age?: number;
 
-    @Prop()
-    gender?: string;
+  @Prop()
+  gender?: string;
 
-    @Prop({ type: [String] })
-    interests?: string[];
+  @Prop({ type: [String] })
+  interests?: string[];
 
-    @Prop()
-    backgroundImage?: string;
+  @Prop()
+  backgroundImage?: string;
 
-    @Prop()
-    birthday?: Date;
+  @Prop()
+  birthday?: Date;
 
-    @Prop()
-    horoscope?: string;
+  @Prop()
+  horoscope?: string;
 
-    @Prop()
-    zodiac?: string;
+  @Prop()
+  zodiac?: string;
 
-    @Prop()
-    height?: string;
+  @Prop()
+  height?: string;
 
-    @Prop()
-    weight?: string;
+  @Prop()
+  weight?: string;
 }
 
 export const ProfileSchema = SchemaFactory.createForClass(Profile);
