df5f270b849c0dc46c20d48841cdf3316d669513
Aldrich <aldrichpinso@gmail.com>
2025-08-18 14:50:37 +0800

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Enable CORS for frontend
+  app.enableCors({
+    origin: 'http://localhost:3008', // your frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, // if you plan to send cookies/auth headers
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
