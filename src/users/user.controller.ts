c4735491c1cc8b0d7f2d3f84c1fe14f3f765105a
Aldrich <aldrichpinso@gmail.com>
2025-08-18 14:51:45 +0800

    feat: user m-s-c

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,7 +1,27 @@
 import { Body, Controller, Post, Get, Query } from "@nestjs/common";
 import { UserService } from "./user.service";
+import { Profile } from "@/schemas/profile.schema";
 
 @Controller('users')
 export class UserController {
-    
+    constructor(private readonly userService: UserService) {}
+
+    @Post('register')
+    async register(@Body() body: any) {
+        const { email, username, password, ...profileData } = body;
+        return this.userService.register(email, username, password, profileData);
+    }
+
+      @Post('login')
+    async login(
+        @Body('identifier') identifier: string,  // email or username
+        @Body('password') password: string
+    ) {
+        return this.userService.login(identifier, password);
+    }
+
+    @Get('find')
+    async findUser(@Query('identifier') identifier: string) {
+        return this.userService.findByEmailOrUsername(identifier);
+    }
 }
--- src/users/user.module.ts
+++ src/users/user.module.ts
@@ -1,18 +1,19 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
-import { User, UserSchema } from "src/schemas/user.schema";
+import { User, UserSchema } from "@/schemas/user.schema";
+import { Profile, ProfileSchema } from "@/schemas/profile.schema";
 import { UserController } from "./user.controller";
 import { UserService } from "./user.service";
 
 @Module({
-    imports: [
-        MongooseModule.forFeature([
-            { name: User.name, schema: UserSchema,}
-        ])
-    ],
-    controllers: [UserController],
-    providers: [UserService],
-    exports: [UserService],
+  imports: [
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: Profile.name, schema: ProfileSchema },
+    ]),
+  ],
+  controllers: [UserController],
+  providers: [UserService],
+  exports: [UserService],
 })
-
 export class UserModule {}
